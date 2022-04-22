66ef87fad2dccad729b5d79697dbcae8e0bdbd5c
Raloooo <optimuslove0223@icloud.com>
2022-04-22 13:32:51 +0900

    (2) second commit

--- decimaltohex.py
+++ decimaltohex.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
-print("input hin number: ", end= '')
-n = int(input())
+print("input bin number: ", end= '')
+n_2 = input()
+new_n2 = "0b" + n_2
+bintodec = int(n_2,2)
 
-print("hexa number:", hex(n))
+
+print("hexa number:", hex(bintodec))
 
